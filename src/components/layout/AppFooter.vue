2270bbc2bfb8d60692247e38bc975e666b719f04
mehrdad sanjari <mehrdadsnj122@gmail.com>
2026-09-12 22:02:44 +0330

    build

--- src/components/layout/AppFooter.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <footer class="footer reveal d4">
-    <span>🎓 University OS</span>
-    <span>۹ درس • ۱۴.۵ ساعت • ترم فعال</span>
-  </footer>
-</template>
